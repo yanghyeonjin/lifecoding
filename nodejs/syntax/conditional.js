2947c861ecab58b070fe6ce6b1536bb51aa9f6af
yanghyeonjin <guswls7081@naver.com>
2020-01-02 18:12:31 +0900

    WEB2 Node.js - 콘솔에서의 입력값

--- nodejs/syntax/conditional.js
+++ nodejs/syntax/conditional.js
@@ -1,6 +1,10 @@
+var args = process.argv;
+console.log(args[2]); // output: [node.js runtime이 어디에 위치하는지, 우리가 실행시킨 파일의 경로, console argument1, console argument2, ...]
+
 console.log("A");
 console.log("B");
-if (true) {
+if (args[2] === "1") {
+    // args[2] 부터 첫번째 인자..
     console.log("C1");
 } else {
     console.log("C2");
