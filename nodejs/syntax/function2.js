9815df925444b568515d16cd0e516f1ae5d00193
yanghyeonjin <guswls7081@naver.com>
2020-01-03 11:04:14 +0900

    WEB2 Node.js - JavaScript 함수의 입력

---
+++ nodejs/syntax/function2.js
@@ -0,0 +1,8 @@
+console.log(Math.round(1.6)); // 2
+console.log(Math.round(1.4)); // 1
+
+function sum(first, second) {
+    // first, second는 parameter
+    console.log(first + second);
+}
+sum(2, 4); // 2, 4는 argument
