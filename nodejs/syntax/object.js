f581d443564fc71c9f118b3da8c7bb307034e009
yanghyeonjin <guswls7081@naver.com>
2020-01-03 15:15:20 +0900

    WEB2 Node.js - JavaScript 객체 vs 배열

---
+++ nodejs/syntax/object.js
@@ -0,0 +1,12 @@
+// 배열
+var members = ["egoing", "k8805", "hoya", "hyeonjin"];
+console.log(members[1]);
+
+// 객체
+var roles = {
+    programmer: "egoing",
+    designer: "k8805",
+    manager: "hoya",
+    me: "hyeonjin"
+};
+console.log(roles.designer);
