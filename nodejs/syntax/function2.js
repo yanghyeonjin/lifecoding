39e1069c2648a87b024820efbbe10cad7d1b6e98
yanghyeonjin <guswls7081@naver.com>
2020-01-03 11:08:08 +0900

    WEB2 Node.js - JavaScript 함수의 출력

--- nodejs/syntax/function2.js
+++ nodejs/syntax/function2.js
@@ -3,6 +3,10 @@ console.log(Math.round(1.4)); // 1
 
 function sum(first, second) {
     // first, second는 parameter
-    console.log(first + second);
+    // console.log(first + second);
+
+    return first + second; // return 시 함수는 종료된다. 따라서 아래 명령은 실행되지 않음.
+    console.log("b");
 }
-sum(2, 4); // 2, 4는 argument
+// sum(2, 4); // 2, 4는 argument
+console.log(sum(2, 4));
