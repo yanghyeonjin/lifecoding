36f8ac7f92366b53701538864834ca9e927d86ba
yanghyeonjin <guswls7081@naver.com>
2020-01-03 10:42:04 +0900

    WEB2 Node.js - JavaScript 함수의 기본 문법

---
+++ nodejs/syntax/function.js
@@ -0,0 +1,22 @@
+f123();
+console.log("Y");
+console.log("A");
+console.log("N");
+console.log("G");
+f123();
+console.log("H");
+console.log("Y");
+console.log("E");
+console.log("O");
+console.log("N");
+f123();
+console.log("J");
+console.log("I");
+console.log("N");
+f123();
+
+function f123() {
+    console.log(1);
+    console.log(2);
+    console.log(3);
+}
