29fd0eaeffd71d1ecd86b9fc5f5728f25007e6e8
yanghyeonjin <guswls7081@naver.com>
2020-01-08 09:31:26 +0900

    Node.js & MySQL - MySQL로 글 수정 기능 구현 2

--- nodejs_mysql/main.js
+++ nodejs_mysql/main.js
@@ -136,14 +136,10 @@ var app = http.createServer(function(request, response) {
         });
         request.on("end", function() {
             var post = qs.parse(body);
-            var id = post.id;
-            var title = post.title;
-            var description = post.description;
-            fs.rename(`data/${id}`, `data/${title}`, function(error) {
-                fs.writeFile(`data/${title}`, description, "utf8", function(err) {
-                    response.writeHead(302, { Location: `/?id=${title}` });
-                    response.end();
-                });
+
+            db.query(`UPDATE topic SET title = ?, description = ?, author_id = 1 WHERE id = ?`, [post.title, post.description, post.id], function(error, result) {
+                response.writeHead(302, { Location: `/?id=${post.id}` });
+                response.end();
             });
         });
     } else if (pathname === "/delete_process") {
