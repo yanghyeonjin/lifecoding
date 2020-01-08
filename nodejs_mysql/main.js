ee8a8f6708585161486dc16d47baba1809735981
yanghyeonjin <guswls7081@naver.com>
2020-01-08 09:26:07 +0900

    Node.js & MySQL - MySQL로 글 수정 기능 구현 1

--- nodejs_mysql/main.js
+++ nodejs_mysql/main.js
@@ -34,7 +34,7 @@ var app = http.createServer(function(request, response) {
                 response.end(html);
             });
         } else {
-            db.query(`SELECT * FROM topic`, function(error, topics, fields) {
+            db.query(`SELECT * FROM topic`, function(error, topics) {
                 if (error) {
                     throw error; // 에러가 있을 경우, 다음 코드를 진행하지 않고 에러를 console에 출력하고 앱을 즉시 종료.
                 }
@@ -100,28 +100,31 @@ var app = http.createServer(function(request, response) {
             });
         });
     } else if (pathname === "/update") {
-        fs.readdir("./data", function(error, filelist) {
-            var filteredId = path.parse(queryData.id).base;
-            fs.readFile(`data/${filteredId}`, "utf8", function(err, description) {
-                var title = queryData.id;
-                var list = template.list(filelist);
+        db.query(`SELECT * FROM topic`, function(error, topics) {
+            if (error) {
+                throw error;
+            }
+            db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id], function(error2, topic) {
+                if (error2) {
+                    throw error2;
+                }
+                var list = template.list(topics);
                 var html = template.HTML(
-                    title,
+                    topic[0].title,
                     list,
-                    `
-            <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${title}">
-              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `,
-                    `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                    `<form action="/update_process" method="post">
+                    <input type="hidden" name="id" value="${topic[0].id}">
+                    <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
+                    <p>
+                      <textarea name="description" placeholder="description">${topic[0].description}</textarea>
+                    </p>
+                    <p>
+                      <input type="submit">
+                    </p>
+                  </form>`,
+                    `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
                 );
+
                 response.writeHead(200);
                 response.end(html);
             });
