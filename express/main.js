d3eb0c54741f39cb002c6efc95d8efe6ff404540
yanghyeonjin <guswls7081@naver.com>
2020-01-10 16:52:24 +0900

    [Express] 라우터 - 파일로 분리

--- express/main.js
+++ express/main.js
@@ -3,11 +3,10 @@ var app = express(); // 변수 express는 함수
 var port = 3000;
 var fs = require("fs");
 var template = require("./lib/template.js");
-var path = require("path");
-var sanitizeHtml = require("sanitize-html");
 var qs = require("querystring");
 var bodyParser = require("body-parser");
 var compression = require("compression");
+var topicRouter = require("./routes/topic");
 
 // public 디렉토리 안에서 정적파일을 찾겠다.
 // url로 localhost:3000/images/파일이름
@@ -37,121 +36,17 @@ app.get("/", (request, response) => {
     response.send(html);
 });
 
-app.get("/topic/create", (request, response) => {
-    var title = "WEB - create";
-    var list = template.list(request.list);
-    var html = template.HTML(
-        title,
-        list,
-        `
-      <form action="/topic/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-      `,
-        ""
-    );
-    response.send(html);
-});
-
-app.post("/topic/create_process", (request, response) => {
-    var post = request.body; // body-parser 미들웨어 사용
-    var title = post.title;
-    var description = post.description;
-    fs.writeFile(`data/${title}`, description, "utf8", function(err) {
-        response.redirect(`/topic/${title}`);
-    });
-});
-
-app.post("/topic/delete_process", (request, response) => {
-    var post = request.body;
-    var id = post.id;
-    var filteredId = path.parse(id).base;
-    fs.unlink(`data/${filteredId}`, function(error) {
-        response.redirect(`/`);
-    });
-});
-
-app.get("/topic/update/:pageID", (request, response) => {
-    var filteredId = path.parse(request.params.pageID).base;
-    fs.readFile(`data/${filteredId}`, "utf8", function(err, description) {
-        var title = request.params.pageID;
-        var list = template.list(request.list);
-        var html = template.HTML(
-            title,
-            list,
-            `
-              <form action="/topic/update_process" method="post">
-                <input type="hidden" name="id" value="${title}">
-                <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-                <p>
-                  <textarea name="description" placeholder="description">${description}</textarea>
-                </p>
-                <p>
-                  <input type="submit">
-                </p>
-              </form>
-              `,
-            `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
-        );
-        response.send(html);
-    });
-});
-
-app.post("/topic/update_process", (request, response) => {
-    var post = request.body;
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    fs.rename(`data/${id}`, `data/${title}`, function(error) {
-        fs.writeFile(`data/${title}`, description, "utf8", function(err) {
-            response.redirect(`/topic/${title}`);
-        });
-    });
-});
-
-app.get("/topic/:pageID", (request, response, next) => {
-    var filteredId = path.parse(request.params.pageID).base;
-    fs.readFile(`data/${filteredId}`, "utf8", function(err, description) {
-        if (err) {
-            next(err);
-        } else {
-            var title = request.params.pageID;
-            var sanitizedTitle = sanitizeHtml(title);
-            var sanitizedDescription = sanitizeHtml(description, {
-                allowedTags: ["h1"]
-            });
-            var list = template.list(request.list);
-            var html = template.HTML(
-                sanitizedTitle,
-                list,
-                `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-                ` <a href="/topic/create">create</a>
-              <a href="/topic/update/${sanitizedTitle}">update</a>
-              <form action="/topic/delete_process" method="post">
-                <input type="hidden" name="id" value="${sanitizedTitle}">
-                <input type="submit" value="delete">
-              </form>`
-            );
-            response.send(html);
-        }
-    });
-});
+// /topic으로 시작하는 주소들에게 topicRouter라고 하는 이름의 미들웨어를 적용하겠다.
+app.use("/topic", topicRouter);
 
 // 404 에러에 대한 커스텀 에러 핸들링
 // 미들웨어는 순차적으로 실행, 404 에러는 페이지를 못 찾았을 때 나오는 에러
-// 코드 맨 위부터 순차적으로 실행하다가 더 이상 못 찾을 때 보여주어야 하기 때문에 마지막에...
-// 코드 중에 next(err)가 있을 땐 실행 안 됨.
+// 코드 맨 위부터 순차적으로 실행하다가 더 이상 못 찾을 때 보여주어야 하기 때문에 마지막 부분에...
 app.use(function(req, res, next) {
     res.status(404).send("Sorry cant find that!");
 });
 
-// 모든 에러에 대한 커스텀 에러 핸들링
+// 커스텀 에러 핸들링
 // 커스텀 에러 핸들링을 작성하게 될 때, next(err)이 호출 될 때 default 에러 핸들링은 모두 무시되고 아래 미들웨어가 실행된다.
 app.use(function(err, req, res, next) {
     console.error(err.stack);
