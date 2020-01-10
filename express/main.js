ba054e635224f36dc97f6f2a421d6d9ecabe0426
yanghyeonjin <guswls7081@naver.com>
2020-01-10 14:20:25 +0900

    [Express] Hello World 1

--- express/main.js
+++ express/main.js
@@ -1,3 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+/*
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
@@ -141,3 +150,4 @@ var app = http.createServer(function(request,response){
     }
 });
 app.listen(3000);
+*/
