867b01e69eceaff603596d780a726696de055e12
yanghyeonjin <guswls7081@naver.com>
2020-01-10 16:37:26 +0900

    [Express] 라우터 - 주소체계변경

--- express/main.js
+++ express/main.js
@@ -33,11 +33,89 @@ app.get("/", (request, response) => {
     var title = "Welcome";
     var description = "Hello, Node.js";
     var list = template.list(request.list);
-    var html = template.HTML(title, list, `<h2>${title}</h2>${description}<img src="/images/hello.jpg" style="width: 300px; display: block; margin-top: 10px;">`, `<a href="/create">create</a>`);
+    var html = template.HTML(title, list, `<h2>${title}</h2>${description}<img src="/images/hello.jpg" style="width: 300px; display: block; margin-top: 10px;">`, `<a href="/topic/create">create</a>`);
     response.send(html);
 });
 
-app.get("/page/:pageID", (request, response, next) => {
+app.get("/topic/create", (request, response) => {
+    var title = "WEB - create";
+    var list = template.list(request.list);
+    var html = template.HTML(
+        title,
+        list,
+        `
+      <form action="/topic/create_process" method="post">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+          <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+      </form>
+      `,
+        ""
+    );
+    response.send(html);
+});
+
+app.post("/topic/create_process", (request, response) => {
+    var post = request.body; // body-parser 미들웨어 사용
+    var title = post.title;
+    var description = post.description;
+    fs.writeFile(`data/${title}`, description, "utf8", function(err) {
+        response.redirect(`/topic/${title}`);
+    });
+});
+
+app.post("/topic/delete_process", (request, response) => {
+    var post = request.body;
+    var id = post.id;
+    var filteredId = path.parse(id).base;
+    fs.unlink(`data/${filteredId}`, function(error) {
+        response.redirect(`/`);
+    });
+});
+
+app.get("/topic/update/:pageID", (request, response) => {
+    var filteredId = path.parse(request.params.pageID).base;
+    fs.readFile(`data/${filteredId}`, "utf8", function(err, description) {
+        var title = request.params.pageID;
+        var list = template.list(request.list);
+        var html = template.HTML(
+            title,
+            list,
+            `
+              <form action="/topic/update_process" method="post">
+                <input type="hidden" name="id" value="${title}">
+                <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                <p>
+                  <textarea name="description" placeholder="description">${description}</textarea>
+                </p>
+                <p>
+                  <input type="submit">
+                </p>
+              </form>
+              `,
+            `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
+        );
+        response.send(html);
+    });
+});
+
+app.post("/topic/update_process", (request, response) => {
+    var post = request.body;
+    var id = post.id;
+    var title = post.title;
+    var description = post.description;
+    fs.rename(`data/${id}`, `data/${title}`, function(error) {
+        fs.writeFile(`data/${title}`, description, "utf8", function(err) {
+            response.redirect(`/topic/${title}`);
+        });
+    });
+});
+
+app.get("/topic/:pageID", (request, response, next) => {
     var filteredId = path.parse(request.params.pageID).base;
     fs.readFile(`data/${filteredId}`, "utf8", function(err, description) {
         if (err) {
@@ -53,93 +131,15 @@ app.get("/page/:pageID", (request, response, next) => {
                 sanitizedTitle,
                 list,
                 `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-                ` <a href="/create">create</a>
-              <a href="/update/${sanitizedTitle}">update</a>
-              <form action="/delete_process" method="post">
+                ` <a href="/topic/create">create</a>
+              <a href="/topic/update/${sanitizedTitle}">update</a>
+              <form action="/topic/delete_process" method="post">
                 <input type="hidden" name="id" value="${sanitizedTitle}">
                 <input type="submit" value="delete">
               </form>`
             );
             response.send(html);
         }
-    });
-});
-
-app.get("/create", (request, response) => {
-    var title = "WEB - create";
-    var list = template.list(request.list);
-    var html = template.HTML(
-        title,
-        list,
-        `
-        <form action="/create_process" method="post">
-          <p><input type="text" name="title" placeholder="title"></p>
-          <p>
-            <textarea name="description" placeholder="description"></textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-        ""
-    );
-    response.send(html);
-});
-
-app.post("/create_process", (request, response) => {
-    var post = request.body; // body-parser 미들웨어 사용
-    var title = post.title;
-    var description = post.description;
-    fs.writeFile(`data/${title}`, description, "utf8", function(err) {
-        response.redirect(`/page/${title}`);
-    });
-});
-
-app.get("/update/:pageID", (request, response) => {
-    var filteredId = path.parse(request.params.pageID).base;
-    fs.readFile(`data/${filteredId}`, "utf8", function(err, description) {
-        var title = request.params.pageID;
-        var list = template.list(request.list);
-        var html = template.HTML(
-            title,
-            list,
-            `
-                <form action="/update_process" method="post">
-                  <input type="hidden" name="id" value="${title}">
-                  <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-                  <p>
-                    <textarea name="description" placeholder="description">${description}</textarea>
-                  </p>
-                  <p>
-                    <input type="submit">
-                  </p>
-                </form>
-                `,
-            `<a href="/create">create</a> <a href="/update/${title}">update</a>`
-        );
-        response.send(html);
-    });
-});
-
-app.post("/update_process", (request, response) => {
-    var post = request.body;
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    fs.rename(`data/${id}`, `data/${title}`, function(error) {
-        fs.writeFile(`data/${title}`, description, "utf8", function(err) {
-            response.redirect(`/page/${title}`);
-        });
-    });
-});
-
-app.post("/delete_process", (request, response) => {
-    var post = request.body;
-    var id = post.id;
-    var filteredId = path.parse(id).base;
-    fs.unlink(`data/${filteredId}`, function(error) {
-        response.redirect(`/`);
     });
 });
 
