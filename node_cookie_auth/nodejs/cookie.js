712145a5e88865148b2534c7ae0ed5ad21e727a5
yanghyeonjin <guswls7081@naver.com>
2020-01-17 16:43:18 +0900

    Node.js 쿠키와 인증 - path & domain

--- node_cookie_auth/nodejs/cookie.js
+++ node_cookie_auth/nodejs/cookie.js
@@ -16,7 +16,9 @@ http.createServer(function(request, response) {
             'tasty_cookie=strawberry',
             `Permanent=cookies; Max-Age=${60 * 60 * 24 * 30}`, // Max-Age: 얼마동안 살아있을 것인가.. 초 단위로 입력
             'SecureCookieName=SecureCookieValue; Secure', // https에서만 쿠키 생성됨
-            'HttpOnlyCookieName=HttpOnlyCookieValue; HttpOnly' // http 통신에서만 쿠키 확인 가능. 자바스크립트를 통해 접근 불가
+            'HttpOnlyCookieName=HttpOnlyCookieValue; HttpOnly', // http 통신에서만 쿠키 확인 가능. 자바스크립트를 통해 접근 불가
+            'PathCookieName=PathCookieValue; Path=/cookie', // 특정 path에서만 쿠키가 살아있도록
+            'Domain=Domain; Domain=o2.org' // 서브 도메인이 와도 살아 남는다.
         ]
     });
     response.end('Cookie!!');
