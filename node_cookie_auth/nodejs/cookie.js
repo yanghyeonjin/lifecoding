4540a9cfa8398588d756a456bbc3fa1790163ad0
yanghyeonjin <guswls7081@naver.com>
2020-01-17 16:18:47 +0900

    Node.js 쿠키와 인증 - 쿠키의 읽기

--- node_cookie_auth/nodejs/cookie.js
+++ node_cookie_auth/nodejs/cookie.js
@@ -1,8 +1,17 @@
 var http = require('http');
+var cookie = require('cookie'); // 쿠키를 다루기 쉽도록 해주는 모듈
+
 http.createServer(function(request, response) {
-    // 서버에서 응답으로 쿠키 설정해주고 주석처리 > reload
-    // response.writeHead(200, {
-    //     'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
-    // });
+    var cookies = {};
+    if (request.headers.cookie !== undefined) {
+        cookies = cookie.parse(request.headers.cookie);
+        console.log(cookies);
+        console.log(cookies.tasty_cookie);
+    }
+    console.log(cookies.yummy_cookie);
+
+    response.writeHead(200, {
+        'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
+    });
     response.end('Cookie!!');
 }).listen(3000);
