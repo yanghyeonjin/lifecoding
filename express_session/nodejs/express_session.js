0f897611004207e3b736da599f4cb0b894d37f06
yanghyeonjin <guswls7081@naver.com>
2020-01-20 14:34:41 +0900

    [Express Session & Auth] session 객체

--- express_session/nodejs/express_session.js
+++ express_session/nodejs/express_session.js
@@ -4,6 +4,7 @@ var session = require('express-session');
 
 var app = express();
 
+// 세션 미들웨어는 request 객체에 session property를 추가해주는구나..
 app.use(
     session({
         secret: 'keyboard cat', // 다른 사람에게 공유되면 안되는 정보
@@ -13,7 +14,15 @@ app.use(
 );
 
 app.get('/', function(req, res, next) {
-    res.send('Hello session');
+    console.log(req.session);
+    if (req.session.num === undefined) {
+        req.session.num = 1;
+    } else {
+        req.session.num += 1;
+    }
+    res.send(`Views: ${req.session.num}`);
+    // 현재 req.session.num은 메모리에 저장된다.
+    // 따라서 서버를 끄면 다 날라감.
 });
 
 app.listen(3000, function() {
