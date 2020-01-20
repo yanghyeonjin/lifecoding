68418d33e5eed8b599ca17ce335b67cbbc671e9a
yanghyeonjin <guswls7081@naver.com>
2020-01-20 14:54:05 +0900

    [Express Session & Auth] 인증구현 - UI 만들기

--- express_session/lib/template.js
+++ express_session/lib/template.js
@@ -8,6 +8,7 @@ module.exports = {
       <meta charset="utf-8">
     </head>
     <body>
+      <a href="/auth/login">login</a>
       <h1><a href="/">WEB</a></h1>
       ${list}
       ${control}
@@ -17,13 +18,13 @@ module.exports = {
     `;
     },
     list: function(filelist) {
-        var list = "<ul>";
+        var list = '<ul>';
         var i = 0;
         while (i < filelist.length) {
             list = list + `<li><a href="/topic/${filelist[i]}">${filelist[i]}</a></li>`;
             i = i + 1;
         }
-        list = list + "</ul>";
+        list = list + '</ul>';
         return list;
     }
 };
