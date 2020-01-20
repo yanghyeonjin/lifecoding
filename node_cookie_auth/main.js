d88d56aed1d81fe67c8f1d3caacf385a1a469a1e
yanghyeonjin <guswls7081@naver.com>
2020-01-20 13:34:34 +0900

    Node.js 쿠키와 인증 - 접근 제어 (인증된 사용자에게만 create, update, delete 권한 주기)

--- node_cookie_auth/main.js
+++ node_cookie_auth/main.js
@@ -77,6 +77,11 @@ var app = http.createServer(function(request, response) {
             });
         }
     } else if (pathname === '/create') {
+        if (authIsOwner(request, response) === false) {
+            response.end('Login required!!');
+            return false;
+        }
+
         fs.readdir('./data', function(error, filelist) {
             var title = 'WEB - create';
             var list = template.list(filelist);
@@ -101,6 +106,11 @@ var app = http.createServer(function(request, response) {
             response.end(html);
         });
     } else if (pathname === '/create_process') {
+        if (authIsOwner(request, response) === false) {
+            response.end('Login required!!');
+            return false;
+        }
+
         var body = '';
         request.on('data', function(data) {
             body = body + data;
@@ -115,6 +125,11 @@ var app = http.createServer(function(request, response) {
             });
         });
     } else if (pathname === '/update') {
+        if (authIsOwner(request, response) === false) {
+            response.end('Login required!!');
+            return false;
+        }
+
         fs.readdir('./data', function(error, filelist) {
             var filteredId = path.parse(queryData.id).base;
             fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
@@ -143,6 +158,11 @@ var app = http.createServer(function(request, response) {
             });
         });
     } else if (pathname === '/update_process') {
+        if (authIsOwner(request, response) === false) {
+            response.end('Login required!!');
+            return false;
+        }
+
         var body = '';
         request.on('data', function(data) {
             body = body + data;
@@ -160,6 +180,11 @@ var app = http.createServer(function(request, response) {
             });
         });
     } else if (pathname === '/delete_process') {
+        if (authIsOwner(request, response) === false) {
+            response.end('Login required!!');
+            return false;
+        }
+
         var body = '';
         request.on('data', function(data) {
             body = body + data;
