669e83214fdc1d3fb905015d339c2e1fcd34f84d
yanghyeonjin <guswls7081@naver.com>
2020-01-21 10:17:00 +0900

    [Express Passport] 리팩토링

--- express_passport/main.js
+++ express_passport/main.js
@@ -9,10 +9,6 @@ var session = require('express-session');
 var FileStore = require('session-file-store')(session); // 파일보단 디비에 저장 권장 ...
 var flash = require('connect-flash');
 
-var topicRouter = require('./routes/topic');
-var indexRouter = require('./routes/index');
-var authRouter = require('./routes/auth');
-
 app.use(helmet()); // 기본적으로 사용한다고 생각
 
 // public 디렉토리 안에서 정적파일을 찾겠다.
@@ -33,72 +29,8 @@ app.use(
 
 app.use(flash()); // session을 사용하기 때문에 session 아래에 배치
 
-var passport = require('passport'), // session 모듈을 사용하기 때문에 use session 아래에 넣어야 한다.
-    LocalStrategy = require('passport-local').Strategy;
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser(function(user, done) {
-    // 로그인 성공했을 때 딱 한번 호출 되면서 session store에 식별자를 저장하는 역할 > sessions 폴더 안에 있는 파일들..
-    // 로그인 성공했을 때 전달한 authData가 user로...
-    // 사용자를 식별할 수 있는 인자를 전달... > 여기에서는 email
-    console.log('Serialize', user);
-    done(null, user.email);
-});
-
-passport.deserializeUser(function(id, done) {
-    // 페이지에 들어올 때마다 호출 됨.
-    // 저장된 데이터를 기준으로 우리가 필요한 데이터를 조회할 때 사용하는 것.
-    // 식별자로 넘겨준 user.email이 id가 되었다.
-    console.log('Deserialize', id);
-    done(null, authData); // authData > request.user로 데이터 주입.
-});
-
-// 실제로 이렇게 하면 안 됨
-var authData = {
-    email: 'egoing777@gmail.com',
-    password: '111111',
-    nickname: 'egoing'
-};
-
-// pm2, nodemon과 같은 자동 재시작 도구를 사용하는 경우 세션 정보가 저장이 안되는 현상이 발생할 수 있습니다.
-// 이유는 sessions 디렉토리에 파일이 추가되면 node가 재시작 되기 때문입니다.
-// 이를 해결하기 위해 sessions 디렉토리에 대해서는 재시작을 하지 않도록 조치해야 합니다.
-// nodemon main.js --ignore sessions/*
-passport.use(
-    new LocalStrategy(
-        {
-            // name으로 넘어오는 단어들 바꾸고 싶을 때...
-            // 기본은 username, password임
-            usernameField: 'email',
-            passwordField: 'pwd'
-        },
-        function(username, password, done) {
-            console.log('LocalStrategy', username, password);
-            if (username === authData.email) {
-                console.log(1);
-                if (password === authData.password) {
-                    console.log(2);
-                    return done(null, authData, { message: 'Welcome!' });
-                } else {
-                    console.log(3);
-                    return done(null, false, { message: 'Incorrect password.' });
-                }
-            } else {
-                console.log(4);
-                return done(null, false, { message: 'Incorrect username.' });
-            }
-        }
-    )
-);
-
-// /auth/login_process로 들어왔을 때 실행
-// 'local' 전략은 username이랑 password로 인증할 때..
-// 구글이나 페이스북 로그인은 다른 것 써야 함
-// 로그인 성공 > /
-// 로그인 실패 > /auth/login
-app.post('/auth/login_process', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/auth/login', failureFlash: true, successFlash: true }));
+// lib/passport가 함수니까 다음과 같이 사용가능
+var passport = require('./lib/passport')(app);
 
 // get 방식에 해당하는 모든 것에 적용
 // post 방식으로 처리하는 것들은 파일 목록을 안불러도 되니까 제외
@@ -108,6 +40,10 @@ app.get('*', function(request, response, next) {
         next();
     });
 });
+
+var topicRouter = require('./routes/topic');
+var indexRouter = require('./routes/index');
+var authRouter = require('./routes/auth')(passport);
 
 // 라우터 분리
 app.use('/', indexRouter);
--- express_passport/routes/auth.js
+++ express_passport/routes/auth.js
@@ -5,18 +5,19 @@ var fs = require('fs');
 var sanitizeHtml = require('sanitize-html');
 var template = require('../lib/template');
 
-router.get('/login', (request, response) => {
-    var fmsg = request.flash();
-    var feedback = '';
-    if (fmsg.error) {
-        feedback = fmsg.error[0];
-    }
-    var title = 'WEB - login';
-    var list = template.list(request.list);
-    var html = template.HTML(
-        title,
-        list,
-        `
+module.exports = function(passport) {
+    router.get('/login', (request, response) => {
+        var fmsg = request.flash();
+        var feedback = '';
+        if (fmsg.error) {
+            feedback = fmsg.error[0];
+        }
+        var title = 'WEB - login';
+        var list = template.list(request.list);
+        var html = template.HTML(
+            title,
+            list,
+            `
         <div style="color: red;">${feedback}</div>
       <form action="/auth/login_process" method="post">
         <p><input type="text" name="email" placeholder="email"></p>
@@ -26,20 +27,27 @@ router.get('/login', (request, response) => {
         </p>
       </form>
       `,
-        ''
-    );
-    response.send(html);
-});
+            ''
+        );
+        response.send(html);
+    });
 
-router.get('/logout', (request, response) => {
-    request.logout(); // passport로 로그아웃하고
-    request.session.save(function() {
-        // 현재의 세션상태를 세션 store에 저장하고 나서...
-        request.session.destroy(function(error) {
-            // 세션 삭제가 끝난 후...
-            response.redirect('/');
+    // /auth/login_process로 들어왔을 때 실행
+    // 'local' 전략은 username이랑 password로 인증할 때..
+    // 구글이나 페이스북 로그인은 다른 것 써야 함
+    // 로그인 성공 > /
+    // 로그인 실패 > /auth/login
+    router.post('/login_process', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/auth/login', failureFlash: true, successFlash: true }));
+
+    router.get('/logout', (request, response) => {
+        request.logout(); // passport로 로그아웃하고
+        request.session.save(function() {
+            // 현재의 세션상태를 세션 store에 저장하고 나서...
+            request.session.destroy(function(error) {
+                // 세션 삭제가 끝난 후...
+                response.redirect('/');
+            });
         });
     });
-});
-
-module.exports = router;
+    return router;
+};
