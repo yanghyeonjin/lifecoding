c25fd15d3ed25b54db5ee154d2133bae00020da5
yanghyeonjin <guswls7081@naver.com>
2020-01-23 14:58:59 +0900

    [Multi User on Passport] 회원가입 UI

--- node_passport_multi_user/routes/auth.js
+++ node_passport_multi_user/routes/auth.js
@@ -39,6 +39,34 @@ module.exports = function(passport) {
     // 로그인 실패 > /auth/login
     router.post('/login_process', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/auth/login', failureFlash: true, successFlash: true }));
 
+    router.get('/register', (request, response) => {
+        var fmsg = request.flash();
+        var feedback = '';
+        if (fmsg.error) {
+            feedback = fmsg.error[0];
+        }
+        var title = 'WEB - login';
+        var list = template.list(request.list);
+        var html = template.HTML(
+            title,
+            list,
+            `
+        <div style="color: red;">${feedback}</div>
+      <form action="/auth/register_process" method="post">
+        <p><input type="text" name="email" placeholder="email"></p>
+        <p><input type="password" name="pwd" placeholder="password"></p>
+        <p><input type="password" name="pwd2" placeholder="password confirm"></p>
+        <p><input type="text" name="displayName" placeholder="displayName"></p>
+        <p>
+          <input type="submit" value="register">
+        </p>
+      </form>
+      `,
+            ''
+        );
+        response.send(html);
+    });
+
     router.get('/logout', (request, response) => {
         request.logout(); // passport로 로그아웃하고
         request.session.save(function() {
