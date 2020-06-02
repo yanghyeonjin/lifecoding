0ec73ffca11a8608750a8c8def088aa0a64a5d12
yanghyeonjin <guswls7081@gmail.com>
2020-06-02 17:42:44 +0900

    [React] - CSS 내용 삭제 (디자인에 집중 X) - 함수 형태가 아닌 클래스 형태로 변경

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello, React!!
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App" >
+        Hello, React!!
+      </div>
+    )
+  }
 }
 
 export default App;
