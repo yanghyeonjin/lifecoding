504cb6c24e037067382166ec66cf9d49e2668a0c
yanghyeonjin <guswls7081@gmail.com>
2020-06-03 15:02:49 +0900

    [React] - create 구현: form

--- react-app/src/components/CreateContent.js
+++ react-app/src/components/CreateContent.js
@@ -5,8 +5,20 @@ class CreateContent extends Component {
         return (
             <article>
                 <h2>Create</h2>
-                <form>
+                <form action="/create_process" method="POST" onSubmit={function (e) {
+                    e.preventDefault();
 
+                    alert('Submit!!!!!')
+                }.bind(this)}>
+                    <p>
+                        <input type="text" name="title" placeholder="title" />
+                    </p>
+                    <p>
+                        <textarea name="desc" placeholder="description"></textarea>
+                    </p>
+                    <p>
+                        <input type="submit" />
+                    </p>
                 </form>
             </article>
         );
