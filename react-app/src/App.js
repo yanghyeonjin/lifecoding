3d12b030996decfcc24ffc3b39d7f5df4c36011d
yanghyeonjin <guswls7081@gmail.com>
2020-06-03 10:41:25 +0900

    [React] - State 사용

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -11,10 +11,21 @@ import Subject from './components/Subject'
 // render라는 method를 가지고 있다.
 // like template
 class App extends Component {
+  // Component가 실행될 때, 제일 먼저 실행된다.
+  // 초기화를 담당
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: "WEB", sub: "World Wide Web!" }
+    }
+  }
+
   render() {
     return (
       <div className="App" >
-        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}></Subject>
         <TOC></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
