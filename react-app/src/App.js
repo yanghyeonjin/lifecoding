7726962d2865e1b1457007cb0d5e191568fd3ff4
yanghyeonjin <guswls7081@gmail.com>
2020-06-03 11:03:57 +0900

    [React] - 이벤트 state props 그리고 render 함수

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -15,8 +15,14 @@ class App extends Component {
   // 초기화를 담당
   constructor(props) {
     super(props);
+
+    // props나 state가 변경되면, 해당 props/state를 가진 컴포넌트의 render함수가 다시 호출된다.
+    // 하위에 있는 컴포넌트들의 render함수도 다시 호출됨.
+    // 페이지 다시 그려짐.
     this.state = {
+      mode: "welcome",
       subject: { title: "WEB", sub: "World Wide Web!" },
+      welcome: { title: "Welcome", desc: "Hello, React!!" },
       contents: [
         { id: 1, title: "HTML", desc: 'HTML is for information' },
         { id: 2, title: "CSS", desc: 'CSS is for design' },
@@ -25,14 +31,25 @@ class App extends Component {
     }
   }
 
+  // render 함수: 어떤 HTML을 그릴것인지 결정하는 함수
   render() {
+    var _title, _desc = null;
+
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+
     return (
       <div className="App" >
         <Subject
           title={this.state.subject.title}
           sub={this.state.subject.sub}></Subject>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     )
   }
--- react-app/src/components/Subject.js
+++ react-app/src/components/Subject.js
@@ -11,7 +11,7 @@ class Subject extends Component {
     render() {
         return (
             <header>
-                <h1>{this.props.title}</h1>
+                <h1><a href="/">{this.props.title}</a></h1>
                 {this.props.sub}
             </header>
         );
