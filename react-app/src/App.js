f809952f9f96c68699aa13c81a3ea7c013f85d84
yanghyeonjin <guswls7081@gmail.com>
2020-06-03 09:44:43 +0900

    [React] - props

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,9 +9,10 @@ class App extends Component {
   render() {
     return (
       <div className="App" >
-        <Subject></Subject>
+        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject title="React" sub="For UI"></Subject>
         <TOC></TOC>
-        <Content></Content>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     )
   }
@@ -28,8 +29,8 @@ class Subject extends Component {
   render() {
     return (
       <header>
-        <h1>WEB</h1>
-        world wide web!
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
       </header>
     );
   }
@@ -55,8 +56,8 @@ class Content extends Component {
   render() {
     return (
       <article>
-        <h2>HTML</h2>
-        HTML is HyperText Markup Language.
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
       </article>
     );
   }
