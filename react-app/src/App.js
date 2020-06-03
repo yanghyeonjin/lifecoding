c6af455430ffcd9fdd99eb18621243e99adea45b
yanghyeonjin <guswls7081@gmail.com>
2020-06-03 09:33:03 +0900

    [React] - 컴포넌트 만들기 2

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -10,6 +10,8 @@ class App extends Component {
     return (
       <div className="App" >
         <Subject></Subject>
+        <TOC></TOC>
+        <Content></Content>
       </div>
     )
   }
@@ -29,6 +31,33 @@ class Subject extends Component {
         <h1>WEB</h1>
         world wide web!
       </header>
+    );
+  }
+}
+
+// Table of Component
+// 목차
+class TOC extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">JavaScript</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML is HyperText Markup Language.
+      </article>
     );
   }
 }
