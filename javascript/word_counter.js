0ac6147b9979c37560e7be4d0b803a416a210713
yanghyeonjin <guswls7081@gmail.com>
2020-06-05 09:14:34 +0900

    JavaScript - Word Counter : 2. 문서에서 문자 가져오기

---
+++ javascript/word_counter.js
@@ -0,0 +1,8 @@
+// 1. 현재 웹페이지의
+// 2. 모든 텍스트에서
+var entireText = document.getElementsByTagName('body')[0].innerText; // body 태그 하위에 있는 모든 텍스트 가져옴.
+
+// 3. 단어들을 쪼갠 후에
+// 4. 등장 횟수를 계산하고
+// 5. 정렬한 후에
+// 6. 콘솔창에 출력한다.
