c4689c0bf3665de40193b8df5ccb4f6428c4e837
yanghyeonjin <guswls7081@gmail.com>
2020-06-05 09:20:46 +0900

    JavaScript - Word Counter : 3. 문자 쪼개기

--- javascript/word_counter.js
+++ javascript/word_counter.js
@@ -3,6 +3,8 @@
 var entireText = document.getElementsByTagName('body')[0].innerText; // body 태그 하위에 있는 모든 텍스트 가져옴.
 
 // 3. 단어들을 쪼갠 후에
+var splitedText = entireText.split(' ');
+
 // 4. 등장 횟수를 계산하고
 // 5. 정렬한 후에
 // 6. 콘솔창에 출력한다.
