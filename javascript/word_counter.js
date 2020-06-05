bb050a515cce360eae509f087d3c9987368293e8
yanghyeonjin <guswls7081@gmail.com>
2020-06-05 09:44:39 +0900

    JavaScript - Word Counter : 6. 등장 빈도 계산하기

--- javascript/word_counter.js
+++ javascript/word_counter.js
@@ -6,8 +6,18 @@ var entireText = document.getElementsByTagName('body')[0].innerText; // body 태
 var splitedText = entireText.split(' ');
 
 // 4. 등장 횟수를 계산하고
-for (var i = 0; i < splitedText.length; i++) {
+var countedWord = {};
 
+for (var i = 0; i < splitedText.length; i++) {
+    var word = splitedText[i];
+
+    if (countedWord[word] == undefined) {
+        // 값이 정해지지 않았다면
+        // 분리한 단어를 property로 주입한 후 1로 초기화 (처음 등장했음을 의미)
+        countedWord[word] = 1;
+    } else {
+        countedWord[word] = countedWord[word] + 1;
+    }
 }
 // 5. 정렬한 후에
 // 6. 콘솔창에 출력한다.
