56135ca4fe95eed4d9a5d195bd6de9d8c0a9ecf7
yanghyeonjin <guswls7081@gmail.com>
2020-06-09 09:29:01 +0900

    겁나 빠른 웹 레시피 - 리로딩 없이 URL 바꾸기 3

---
+++ fast_web_recipe/reloadless.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.control ul li a').on('click', function (e) {
+        e.preventDefault();
+
+        // 실제 페이지 이동 없이 url만 변경
+        history.pushState(null, null, e.target.href);
+
+        // e.target.href에 해당하는 웹페이지에서 article 직계자손 중 .content를 가져온다. (태그를 포함)
+        // article 아래 내용이 .content 태그로 바뀜.
+        $('article').load(e.target.href + ' article>.content');
+    })
+})
