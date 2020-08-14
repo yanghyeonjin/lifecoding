f046f2db60328c3f65229ffa48e1967aa67f229d
yanghyeonjin <guswls7081@gmail.com>
2020-08-14 10:57:55 +0900

    * React Redux - 6.3. React Redux - mapStateToProps

--- react/react-redux/src/containers/DisplayNumberContainer.jsx
+++ react/react-redux/src/containers/DisplayNumberContainer.jsx
@@ -1,6 +1,25 @@
 import DisplayNumber from '../components/DisplayNumber';
 import { connect } from 'react-redux';
-export default connect()(DisplayNumber);
+
+// 리덕스의 state를 리액트의 props로...
+// 아래 함수는 다음과 같다.
+// 1. 리덕스의 state를 가져온다.
+// 2. 리덕스의 state가 변경되었을 때 함수가 다시 호출된다. (UI 업데이트)
+// 3. 하위 컴포넌트에게 props로 값을 전달해준다.
+//      - 전달하려는 property 값을 일치시켜야 한다.
+function mapReduxStateToReactProps(state) {
+    return {
+        number: state.number,
+    };
+}
+function mapReduxDispatchToReactProps() {
+    return {};
+}
+
+export default connect(
+    mapReduxStateToReactProps,
+    mapReduxDispatchToReactProps
+)(DisplayNumber);
 
 /*
 import React, { Component } from 'react';
